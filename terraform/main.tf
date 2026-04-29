513df0bbdc7212f37a5b8e699b90217901a296a2
Jatin4599 <jatin@gmail.com>
2026-04-29 18:25:58 +0530

    feat: terraform ACR infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "todo-api-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "acr_name" {
+  default = "todoapijatin4599"
+}
+
+variable "app_service_plan_name" {
+  default = "todo-api-plan"
+}
+
+variable "app_service_name" {
+  default = "todo-api-jatin4599"
+}
